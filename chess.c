3f0cadc46537b8ba70c012cc95cfa0179109fa02
RJ Swedlow <rj150@hotmail.com>
2012-08-03 12:38:29 -0500

    stupid Visual Studio spacing

---
+++ chess.c
@@ -0,0 +1,110 @@
+#define ERROR -1
+#define WHITE  0
+#define BLACK  1
+
+#include <stdio.h> // not that I'm even using anything, except printf()
+#include <windows.h> // not using anything here either yet, keep portable?
+// lol, C++-style comments with C-style include directives, anyone?
+
+char game[8][8] =
+{ /* QR    QN    QB     Q     K    KB    KN    KR */
+    0x02, 0x04, 0x23, 0x01, 0x00, 0x03, 0x24, 0x02, /* rank 1 */
+    0x05, 0x05, 0x05, 0x05, 0x05, 0x05, 0x05, 0x05, /* rank 2 */
+    0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, /* rank 3 */
+    0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, /* rank 4 */
+    0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, /* rank 5 */
+    0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, /* rank 6 */
+    0x15, 0x15, 0x15, 0x15, 0x15, 0x15, 0x15, 0x15, /* rank 7 */
+    0x12, 0x14, 0x33, 0x11, 0x10, 0x13, 0x34, 0x12  /* rank 8 */
+};
+/* What's with all those hex bytes?  I invented this convention:
+        0x?1 = Queen, 0x?2 = Rook, 0x?3 = Bishop, 0x?4 = Knight,
+        (Just like in the non-standard ICF numeric notation for promotions!)
+        0x?0 = King, 0x?5 = Pawn, um, for blank squares, 0x?F I guess (change?)
+        0x?X, where X > 5 = reserved for vendor use (Invent your own pieces!)
+
+   My choice of the high-order nybble flags of all those bytes:
+        0001:  The piece is Black, not White (0x10 Black King, 0x00 White King).
+        0010:  valid castling destination square (for Chess960, can be any sq.)
+        0100:  valid en passant square (pawn capturing, just the ranks 3 and 6)
+        1000:  I guess, reserved .. might be used for draw rules?
+*/
+
+unsigned load_FEN(char FEN[96]);
+int main()
+{
+    char board[128] = { /* ASCII chess board testing */
+        'r',' ','n',' ','b',' ','q',' ','k',' ','b',' ','n',' ','r',0xA,
+        'p',' ','p',' ','p',' ','p',' ','p',' ','p',' ','p',' ','p',0xA,
+        '.',' ','.',' ','.',' ','.',' ','.',' ','.',' ','.',' ','.',0xA,
+        '.',' ','.',' ','.',' ','.',' ','.',' ','.',' ','.',' ','.',0xA,
+        '.',' ','.',' ','.',' ','.',' ','.',' ','.',' ','.',' ','.',0xA,
+        '.',' ','.',' ','.',' ','.',' ','.',' ','.',' ','.',' ','.',0xA,
+        'P',' ','P',' ','P',' ','P',' ','P',' ','P',' ','P',' ','P',0xA,
+        'R',' ','N',' ','B',' ','Q',' ','K',' ','B',' ','N',' ','R',0x0 };
+    printf(board);
+    return 0;
+}
+
+unsigned load_FEN(char FEN[96])
+{
+    register int i = 0; /* array indexing loop variable */
+    register int file = 000; /* file A, second dimension of the game[] array */
+    register int rank = 000; /* rank 1, first dimension of game array access */
+    register unsigned RET_SLOT; /* 16-bit integer with game property flags */
+                                /* flag whether it's Black's turn, castling,
+                                /* etc., just not implemented here yet .. */
+
+    if (FEN[0] < '-') /* in FEN, ASCII code points before this invalid */
+    {
+        return (ERROR);
+    } /* Check just first character to split from the following loop. */
+
+    FEN[92] =  ' '; /* to prevent the next loop from looping indefinitely */
+    FEN[93] = '\r'; /* Windows new line format:  CRLF */
+    FEN[94] = '\n'; /* '\r' is the carriage return; '\n' is the line feed. */
+    FEN[95] = '\0'; /* standard, necessary to prevent indefinite recursion */
+    do
+    {
+        register int char_reg = FEN[i];
+        if (char_reg == ' ')
+        {
+            ++i; /* Skip the space character before advance. */
+            break;
+        } /* in MIPS:  BEQ FEN[i], 32, next_block; */
+
+        if (char_reg & 0x0030 == 0x0030) /* a possible decimal digit in ASCII */
+        {
+            register unsigned square_offset = char_reg & 0x000F;
+            if (square_offset > 8) /* char_reg not between '0' and '8' */
+            {
+                return (ERROR);
+            }
+            if (file + square_offset > 7) /* outside the chessboard boundary */
+            { /* (`file` is the existing offset onto current row of squares.) */
+                return (ERROR);
+            }
+            file = file + square_offset;
+    }
+    else
+    {
+            if (char_reg == '/') /* divider between chessboard ranks */
+            {
+                ++rank;
+                if (rank == 8) /* the ninth rank, given zero base */
+                {
+                    return (ERROR);
+                }
+            }
+            else
+            {
+                /* It must be a piece letter.  When I finish this, the ASCII  *
+                 * code point will be translated into which piece code to     *
+                 * write at the registered game[] chessboard index to load.   */
+            }
+        }
+        ++i;
+    } while (0 == 0); /* See the "in MIPS..." comment; I prefer BEQ over BNE. */
+    /* I need to decide how to define the RET_SLOT to send to parent block. */
+    return (RET_SLOT);
+}
