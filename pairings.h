fa1884c2f632f12109a952c79f40b4ec7f7f043c
RJ Swedlow <rj150@hotmail.com>
2012-09-11 16:35:29 -0500

    free piece of shit to fix later

--- pairings.h
+++ pairings.h
@@ -9,12 +9,13 @@
 
 extern unsigned int count_participants(char *string);
 extern unsigned int valid_pairs_count(char *string);
-extern unsigned int valid_pairs_count_Thibault(char *string); /* his idea xD */
-extern void valid_pairs_show(char *string);
+extern unsigned int valid_pairs_show(char *string);
+extern void valid_rounds_show(char *string); /* experimental */
 
-void valid_pairs_show(char *figures)
+unsigned int valid_pairs_show(char *string)
 {
     register unsigned int i = 0x0000;
+    register unsigned int tables = 0x0000;
     do
     {
         register unsigned int count = i;
@@ -22,14 +23,15 @@ void valid_pairs_show(char *figures)
         do
         {
             char pairing[8] = "(?, ?)\n";
-            pairing[1] = figures[i];
-            pairing[4] = figures[count++];
+            pairing[1] = string[i];
+            pairing[4] = string[count++];
             printf(pairing);
-        } while (figures[count] != '\0');
+            ++tables;
+        } while (string[count] != '\0');
         ++i;
-    } while (figures[i + 1] != '\0');
+    } while (string[i + 1] != '\0');
+    return (tables);
 }
-
 unsigned int count_participants(char *string)
 {
     register unsigned int i = 0x0000;
@@ -45,27 +47,67 @@ unsigned int valid_pairs_count(char *string)
 {
     register unsigned int str_len = count_participants(string);
     register unsigned int count = 0x0000;
-    while (str_len != 0)
+    while (str_len != 0x0001)
     {
-        --str_len; /* (n - 1) */
+        --str_len;
         count += str_len;
     }
     return (count);
 }
 
-unsigned int valid_pairs_count_Thibault(char *string)
+void valid_rounds_show(char *string)
 {
-    register unsigned int str_len = count_participants(string);
-    if (str_len = 0x0000) return 0;
-    else
+    FILE *stream = fopen("pairings.txt", "w");
+    char *buffer;
+    register unsigned int i = 0x0000;
+    do
     {
-        register unsigned int count = 0x0001;
-        register unsigned int inc = 0x0001;
-        while (inc < str_len)
+        fprintf(stream, "[");
         {
-            inc += 2;
-            count *= inc;
-        }
-        return (count);
-    }
+            char *buffer;
+            register unsigned int count = i;
+            do
+            {
+                buffer[i] = string[i];
+                ++i;
+            } while (string[i] != '\0');
+            buffer[i] = '\0';
+
+            ++count;
+            do
+            {
+                char pairing[8] = "(?, ?),";
+                while (buffer[i] == -1)
+                {
+                    ++i;
+                    if (buffer[i] == '\0')
+                    {
+                        fprintf(stream, "]");
+                        fprintf(stream, ";");
+                        fprintf(stream, "\n");
+                        fclose(stream);
+                        return;
+                    }
+                }
+                pairing[1] = buffer[i];
+                buffer[i] = -1;
+                while (buffer[count] == -1)
+                {
+                    ++count;
+                    if (buffer[count] == '\0')
+                    {
+                        fprintf(stream, "]");
+                        fprintf(stream, ";");
+                        fprintf(stream, "\n");
+                        fclose(stream);
+                        return;
+                    }
+                }
+                pairing[4] = buffer[count];
+                buffer[count] = -1;
+                ++count;
+                fprintf(stream, pairing);
+            } while (count == count);
+        } while (0 == 0);
+    } while (0 == 0);
 }
