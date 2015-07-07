03ea2269ae3f2ab4a3b88f71d219a2251ce519da
unknown <rj150@hotmail.com>
2015-07-07 17:38:49 -0400

    prepared mutual test position where either side can mate in one

--- main.c
+++ main.c
@@ -13,6 +13,17 @@ int main(int argc, char* argv[])
     else
         ret_val = load_FEN(argv[1]);
 
+#if defined(TEST_MATE_IN_ONE)
+    memcpy(&square(a8), "nk....bq", 8);
+    memcpy(&square(a7), "ppp.r...", 8);
+    memcpy(&square(a6), "........", 8);
+    memcpy(&square(a5), "........", 8);
+    memcpy(&square(a4), "........", 8);
+    memcpy(&square(a3), "........", 8);
+    memcpy(&square(a2), "...R.PPP", 8);
+    memcpy(&square(a1), "QB....KN", 8);
+#endif
+
     if (fen_status != FEN_OK)
     {
         fprintf(stderr, "(%i):  FEN record is corrupt!\n\n", fen_status);
