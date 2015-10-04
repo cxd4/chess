efc8f8ae2c9569943920ff897df885c2c33bc56a
no <rj150@hotmail.com>
2015-10-03 23:14:05 -0400

    working Unix make script

---
+++ make.sh
@@ -0,0 +1,20 @@
+mkdir -p obj
+src="."
+
+FLAGS_C="-masm=intel -std=c89 -pedantic -ansi -Wall"
+OBJ_LIST="obj/main.o obj/move.o obj/check.o obj/forsyth.o"
+
+echo Compiling C sources...
+cc -S $FLAGS_C -o obj/main.asm    -Os   $src/main.c
+cc -S $FLAGS_C -o obj/move.asm    -O3   $src/move.c
+cc -S $FLAGS_C -o obj/check.asm   -O3   $src/check.c
+cc -S $FLAGS_C -o obj/forsyth.asm -Os   $src/forsyth.c
+
+echo Assembling compiled sources...
+as -o obj/main.o                        obj/main.asm
+as -o obj/move.o                        obj/move.asm
+as -o obj/check.o                       obj/check.asm
+as -o obj/forsyth.o                     obj/forsyth.asm
+
+echo Linking assembled objects...
+cc -s -o chess $OBJ_LIST -lm
